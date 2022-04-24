49fbb17bc65f6986e2c28bc10254ac8d4c786821
Aditya Kanu <adityakanu1st@gmail.com>
2022-04-24 19:41:22 +0530

    Check

--- Day1_Ass2.c
+++ Day1_Ass2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main(){
- 
-    
+
+
     int a;
     float myFloat;
     char myChar1;
-    
+
 
     scanf ("%c",& myChar1);
     scanf ("%d",& a);
     scanf ("%f",& myFloat);
+    
     printf ("This is the character'%c'\n", myChar1);
     printf ("This is the int '%d'\n", a);
     printf ("This is the float'%f'\n", myFloat);
 
- 
+
 return 0;
 }
